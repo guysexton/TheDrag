b537336e65a756a1da97a34daac527c973c4ce88
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-03-04 18:35:35 -0600

    testing csv for dealerships

--- src/main/java/thedrag/ParseCSVLineByLine.java
+++ src/main/java/thedrag/ParseCSVLineByLine.java
@@ -1,8 +1,19 @@
 package thedrag;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.opencsv.*;
 
 public class ParseCSVLineByLine
@@ -12,7 +23,7 @@ public class ParseCSVLineByLine
 	   { 
 		   
 		    try { 
-		  
+		    	
 		        // Create an object of filereader 
 		        // class with CSV file as a parameter. 
 		        FileReader filereader = new FileReader("Honda Pilot EX.csv"); 
@@ -28,7 +39,42 @@ public class ParseCSVLineByLine
 		                System.out.print(cell + "\t"); 
 		            } 
 		            System.out.println(); 
-		        } 
+		        }
+		        BufferedReader br = new BufferedReader(new FileReader("Honda Pilot EX.csv"));
+		        String line;
+		        line = br.readLine();
+		        while ((line = br.readLine()) != null) {
+		            // use comma as separator
+		            String[] cols = line.split(",");
+		            String[] dealer = cols[4].split("(?<=by) ");
+		            System.out.println(dealer[1]);
+		        }
+		        
+		    	/*
+		    	BufferedReader br = new BufferedReader(new FileReader("Honda Pilot EX.csv"));
+		        String line;
+		        line = br.readLine();
+		        boolean present = false;
+		        String dealers = "dealers";
+				Key dealKey = KeyFactory.createKey("DealKey", dealers);
+				Entity dealer = new Entity("Dealer", dealKey);
+				DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+				Query subQuery = new Query("Dealer", dealKey);
+				List<Entity> dealList = datastore.prepare(subQuery).asList(FetchOptions.Builder.withDefaults());
+				while ((line = br.readLine()) != null) {
+		            // use comma as separator
+					String[] cols = line.split(",");
+		            String[] dealerName = cols[4].split("(?<=by) ");
+		            dealer.setProperty("name", dealerName[1]);
+		            for (Entity d: dealList) {
+						if(d.getProperty("name").equals(cols[4])) {
+							present = true;
+						}
+					}
+		            if(!present) {
+		            	datastore.put(dealer);
+		            }
+		        }*/
 		    } 
 		    catch (Exception e) { 
 		        e.printStackTrace(); 
