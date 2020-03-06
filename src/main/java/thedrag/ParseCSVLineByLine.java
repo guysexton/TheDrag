a22b582a6c938b3cc7a3cf268a23657e0a0a92c3
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-03-05 20:45:46 -0600

    added some stuff to run with google sdk shell

--- src/main/java/thedrag/ParseCSVLineByLine.java
+++ src/main/java/thedrag/ParseCSVLineByLine.java
@@ -1,19 +1,7 @@
 package thedrag;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.List;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.opencsv.*;
 
 public class ParseCSVLineByLine
