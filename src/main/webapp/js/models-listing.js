9ee19b300d1768f72875e26d182f0ead763ed4d4
Daniel Lazcano <daniellazcanoplus@gmail.com>
2020-03-29 13:44:55 -0500

    Models Listing Page Code
    Running into issue with cards overlapping and the page buttons not working.

---
+++ src/main/webapp/js/models-listing.js
@@ -0,0 +1,289 @@
+// JavaScript Document - MODELS listing
+
+const models = [{
+    "name" : "2020 BMW M2 Competition",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/440595/WBS2U7C06L7F11298/10130e51db7f61303ebf08d970a090f5.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/795726481/overview/",
+    "vin" : "WBS2U7C06L7F11298",
+    "dealership" : "BMW of Austin",
+    "make" : "BMW",
+    "price" : 66210,
+    "mpg" : "18city/25hwy"
+  },
+  {
+    "name" : "2019 Audi Q7 55 Premium Plus",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/18588981/WA1LAAF78KD016330/1f51640cebe6189512d529f59aeef0d3.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/782615667/overview/",
+    "vin" : "WA1LAAF78KD016330",
+    "dealership" : "Audi North Austin",
+    "make" : "Audi",
+    "price" : 71840,
+    "mpg" : null
+  },
+  {
+    "name" : "2020 Nissan Rogue Sport S",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/431031/JN1BJ1CV2LW255062/0f1d4e3bd3f738f9d78982125745e50d.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/798234033/overview/",
+    "vin" : "JN1BJ1CV2LW255062",
+    "dealership" : "Town North Nissan",
+    "make" : "Nissan",
+    "price" : 20826,
+    "mpg" : "24city/30hwy"
+  },
+  {
+    "name" : "2020 Buick Encore Preferred",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/415830/KL4CJASB7LB000109/1f9f00023fbbbeeb8a34635c19295984.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/794678900/overview/",
+    "vin" : "KL4CJASB7LB000109",
+    "dealership" : "Covert Buick GMC Austin",
+    "make" : "Buick",
+    "price" : 19090,
+    "mpg" : "25city/30hwy"
+  },
+  {
+    "name" : "2020 Ford Mustang EcoBoost Premium",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/439165/1FA6P8TH7L5146379/09d7b59695d71031973849fb77b2f437.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/807449936/overview/",
+    "vin" : "1FA6P8TH7L5146379",
+    "dealership" : "Maxwell Ford Supercenter",
+    "make" : "Ford",
+    "price" : 28233,
+    "mpg" : "21city/30hwy"
+  },
+  {
+    "name" : "2020 Honda Accord LX 1.5T",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/422726/1HGCV1F17LA057137/68364ab1ed1a2ae42bb2e54a18f6a0f5.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/801985479/overview/",
+    "vin" : "1HGCV1F17LA057137",
+    "dealership" : "Howdy Honda",
+    "make" : "Honda",
+    "price" : 24975,
+    "mpg" : "30city/38hwy"
+  },
+  {
+    "name" : "2020 Subaru Outback Premium",
+    "img" : "https://www.cstatic-car-pictures.com/images/?IMG=USD00SUS311B022003.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/806845605/overview/",
+    "vin" : "4S4BTAEC7L3212871",
+    "dealership" : "CAG - Austin Subaru",
+    "make" : "Subaru",
+    "price" : 31416,
+    "mpg" : "26city/33hwy"
+  },
+  {
+    "name" : "2020 Volvo XC90 T6 Momentum 7 Passenger",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/431356/YV4A22PK7L1575015/7a420f85efcd90bed4fe5cfb0f8d5160.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/796277685/overview/",
+    "vin" : "YV4A22PK7L1575015",
+    "dealership" : "Volvo Cars of Austin",
+    "make" : "Volvo",
+    "price" : 56125,
+    "mpg" : "18city/26hwy"
+  },
+  {
+    "name" : "2020 Chevrolet Trax LS",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/425771/3GNCJKSB0LL228714/b33101b88838103242a89555cb321fb7.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/798448634/overview/",
+    "vin" : "3GNCJKSB0LL228714",
+    "dealership" : "Henna Chevrolet",
+    "make" : "Chevrolet",
+    "price" : 15999,
+    "mpg" : "26city/31hwy"
+  },
+  {
+    "name" : "2020 Volvo XC40 T5 Momentum",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/425771/3GNCJKSB0LL228714/b33101b88838103242a89555cb321fb7.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/806687663/overview/",
+    "vin" : "YV4162UK0L2299005",
+    "dealership" : "Volvo Cars of Austin",
+    "make" : "Volvo",
+    "price" : 42610,
+    "mpg" : "22city/30hwy"
+  },
+  {
+    "name" : "2020 Kia Forte LXS",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/17853346/3KPF24AD0LE194358/1ae777cf792f023c8bdbb586458b52ee.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/795309525/overview/",
+    "vin" : "3KPF24AD0LE194358",
+    "dealership" : "KIA of South Austin",
+    "make" : "Kia",
+    "price" : 16998,
+    "mpg" : "29city/40hwy"
+  },
+  {
+    "name" : "2020 Hyundai Tucson Value",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/11143679/KM8J33A46LU169303/ed2103e204a5c078b7959d51cb428c9e.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/799146333/overview/",
+    "vin" : "KM8J33A46LU169303",
+    "dealership" : "Round Rock Hyundai",
+    "make" : "Hyundai",
+    "price" : 26460,
+    "mpg" : "23city/28hwy"
+  },
+  {
+    "name" : "2020 Hyundai Ioniq Hybrid SE",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/2320376/KMHC75LC3LU190235/cc925abb1ee32b5a3a0134be9adda96d.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/805161923/overview/",
+    "vin" : "KMHC75LC3LU190235",
+    "dealership" : "South Point Hyundai",
+    "make" : "Hyundai",
+    "price" : null,
+    "mpg" : "55city/54hwy"
+  },,
+  {
+    "name" : "2020 Genesis G70 2.0T Sport",
+    "img" : "https://www.cstatic-images.com/supersized/in/v1/2320376/KMTG24LA0LU064431/1cbc2d4a29d384fbdaaeaa79f8d19e5f.jpg",
+    "url" : "https://www.cars.com/vehicledetail/detail/805574582/overview/",
+    "vin" : "KMTG24LA0LU064431",
+    "dealership" : "South Point Hyundai",
+    "make" : "Genesis",
+    "price" : null,
+    "mpg" : "18city/28hwy"
+  }
+]
+
+var state = {
+		'querySet': dealers,
+		
+		'page':1,
+		'items':10,
+		
+		'window':5
+	}
+
+	buildGrid()
+
+	function pagination(querySet,page,items) {
+		var trimStart = (page - 1) * items
+		var trimEnd = trimStart + items
+		
+		var trimmedData = Array.from(querySet).slice(trimStart, trimEnd)
+		
+		var pages = Math.ceil(querySet.length/items)
+		
+		return{
+			'querySet':trimmedData,
+			'pages':pages
+		}
+	}
+
+
+function pageButtons(pages){
+	
+	var winStart = (state.page - Math.floor(state.window/2));
+	var winEnd = (state.page + Math.floor(state.window/2));
+	
+	if(winStart < 1){
+		winStart=1
+		winEnd=state.window
+	}
+	
+	if(winEnd > pages){
+		winStart = pages - (state.window - 1)
+		
+		if(winStart < 1){
+			winStart=1
+		}
+		winEnd=pages
+	}
+	
+	var wrapper = document.getElementById('pagination-wrapper')
+	wrapper.innerHTML = ''
+	
+		if(state.page>1){
+		wrapper.innerHTML += `<li class="page-item"> <button class="first np-element np-hover" style="margin:5px;" href="#" aria-label="Previous"> <span aria-hidden="true">&laquo;</span> <span class="sr-only">First</span> </button> </li>`}
+		else{
+			wrapper.innerHTML += `<li class="page-item"> <button class="first np-element" style="margin:5px;" href="#" aria-label="Previous" disabled> <span aria-hidden="true">&laquo;</span> <span class="sr-only">First</span> </button> </li>`
+		}
+	
+	wrapper.innerHTML += `<li class="page-item"> <button class="prev np-element np-hover" style="margin:5px;" href="#" aria-label="Previous"> <span aria-hidden="true">&lt;</span> <span class="sr-only">Previous</span> </button> </li>`
+	
+	for(var page = winStart; page <= winEnd ; page++){
+		if(page==state.page)
+			wrapper.innerHTML += `<li><button value=${page} class="np-element np-shadow-inverse np-hover-inverse" style="margin:5px;">${page}</button></li>`
+		else
+			wrapper.innerHTML += `<li><button value=${page} class="page np-element np-hover" style="margin:5px;">${page}</button></li>`
+	}
+	
+		wrapper.innerHTML += `<li class="page-item"> <button class="next np-element np-hover" style="margin:5px;" href="#" aria-label="Next"> <span aria-hidden="true">&gt;</span> <span class="sr-only">Next</span> </button> </li>`
+	
+		if(state.page<pages){
+		wrapper.innerHTML += `<li class="page-item"> <button class="last np-element np-hover" style="margin:5px;" href="#" aria-label="Next"> <span aria-hidden="true">&raquo;</span> <span class="sr-only">Last</span> </button> </li>`}
+		else{
+			wrapper.innerHTML += `<li class="page-item"> <button class="last np-element" style="margin:5px;" href="#" aria-label="Next" disabled> <span aria-hidden="true">&raquo;</span> <span class="sr-only">Last</span> </button> </li>`
+		}
+	
+	$('.page').on('click',function(){
+		document.getElementById('model-grid').innerHTML=''
+		
+		state.page = Number($(this).val())
+		
+		buildGrid()
+	})
+	
+	$('.prev').on('click',function(){
+		document.getElementById('model-grid').innerHTML=''
+		
+		if(state.page>1){state.page = state.page-1}
+		else{state.page=pages}
+		
+		buildGrid()
+	})
+	
+	$('.next').on('click',function(){
+		document.getElementById('model-grid').innerHTML=''
+		
+		if(state.page<pages){state.page = state.page+1}
+		else{state.page=1}
+		
+		buildGrid()
+	})
+	
+	$('.first').on('click',function(){
+		document.getElementById('model-grid').innerHTML=''
+		
+		state.page=1
+		
+		buildGrid()
+	})
+	
+	$('.last').on('click',function(){
+		document.getElementById('model-grid').innerHTML=''
+		
+		state.page=pages
+		
+		buildGrid()
+	})
+}
+
+
+
+function buildGrid() {
+	var console = window.console
+	console.log(state.page)
+	
+	var grid = $('#model-grid')
+	
+	var data = pagination(state.querySet,state.page, state.items)
+	
+	var myGrid = data.querySet
+	
+	for(var i = 1 in myGrid){
+		var listing = "<li class='card np-element np-hover col-4 model-card' style='margin: 20px;height:275px;' >"+
+						"<a href='#'>"+
+						"<h3 style='text-align: center;'>" + myGrid[i].name + "</h3>"
+		
+		if(myGrid[i].img!=""){ listing = listing + "<div class='np-img-wrapper' width='50px' height='50px'>"+
+							"<img class='np-img-expand' src='" + myGrid[i].img + "' width='inherit' height='inherit' style='margin: 10px'></div>"}
+		
+		if(myGrid[i].address!=""){ listing = listing + "<p><strong>Dealership:</strong> " + myGrid[i].dealership + "</p>"}
+		if(myGrid[i].phoneNum!=""){ listing = listing + "<p><strong>Price:</strong> " + myGrid[i].price + "</p>"}
+		if(myGrid[i].website!=""){ listing = listing + "<a href='" + myGrid[i].url + "'><strong>Check Out Dealership Listing</strong></a>"}
+		listing = listing + "</a> </li>"
+						
+		
+		grid.append(listing)
+	}
+
+	pageButtons(data.pages)
+}
