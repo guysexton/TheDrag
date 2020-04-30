728a29a65b8ec6eef85629633324b614cc36a589
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-04-29 22:20:05 -0500

    Refactored Make.java Encapsulate Collection

--- src/main/java/web_scraping/Make.java
+++ src/main/java/web_scraping/Make.java
@@ -12,7 +12,7 @@ public class Make {
 	String market;
 	String years;
 	String url;
-	
+
 	Make(){
 		this.name = null;
 		this.img = null;
@@ -29,5 +29,77 @@ public class Make {
 		return "Make: " + name + " | " + "Image: " + img + " | " + "Dealerships: " + dealerships 
 				+ " | " + "Number of Dealerships: " + numDealerships + " | " + "Cars: " + cars + " | " + "Number of Cars: " + numCars
 				+ " | " + "Market: " + market + " | " + "Years: " + years + " | " + "url: " + url;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public HashSet<String> getDealerships() {
+		return dealerships;
+	}
+
+	public void setDealerships(HashSet<String> dealerships) {
+		this.dealerships = dealerships;
+	}
+
+	public ArrayList<String> getCars() {
+		return cars;
+	}
+
+	public void setCars(ArrayList<String> cars) {
+		this.cars = cars;
+	}
+
+	public Integer getNumCars() {
+		return numCars;
+	}
+
+	public void setNumCars(Integer numCars) {
+		this.numCars = numCars;
+	}
+
+	public Integer getNumDealerships() {
+		return numDealerships;
+	}
+
+	public void setNumDealerships(Integer numDealerships) {
+		this.numDealerships = numDealerships;
+	}
+
+	public String getMarket() {
+		return market;
+	}
+
+	public void setMarket(String market) {
+		this.market = market;
+	}
+
+	public String getYears() {
+		return years;
+	}
+
+	public void setYears(String years) {
+		this.years = years;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 }
