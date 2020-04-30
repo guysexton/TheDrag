06a3df337b569cd19fdc550b5c98ffb087b91dfa
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-04-29 22:15:04 -0500

    Refactored Car.java Encapsulate Collection

--- src/main/java/web_scraping/Car.java
+++ src/main/java/web_scraping/Car.java
@@ -1,14 +1,14 @@
 package web_scraping;
 
 public class Car{
-	String name;
-	String img;
-	String url; 
-	String vin; 
-	String dealership; 
-	String make;
-	Integer price;
-	String mpg;
+	private String name;
+	private String img;
+	private String url; 
+	private String vin; 
+	private String dealership; 
+	private String make;
+	private Integer price;
+	private String mpg;
 
 	Car(){
 		this.name = null;
@@ -35,5 +35,69 @@ public class Car{
 	public String toString() {
 		return "Car Name: " + name + " | " + "Car Image: " + img + " | " + "Car URL: " + url + " | " + "Car VIN: " + vin + " | " 
 					+ "Dealership: " + dealership + " | " + "Make: " + make + " | " + "Price: " + price + " | " + "MPG: " + mpg;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+	public String getDealership() {
+		return dealership;
+	}
+
+	public void setDealership(String dealership) {
+		this.dealership = dealership;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getMpg() {
+		return mpg;
+	}
+
+	public void setMpg(String mpg) {
+		this.mpg = mpg;
 	}
 }
