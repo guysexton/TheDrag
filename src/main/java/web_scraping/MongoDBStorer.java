f4318b56b7a972eb7036244a0966628eb1b82167
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-04-30 00:23:17 -0500

    Extract method refactoring of MongoDBStorer.java

--- src/main/java/web_scraping/MongoDBStorer.java
+++ src/main/java/web_scraping/MongoDBStorer.java
@@ -28,91 +28,81 @@ import static com.mongodb.client.model.Filters.eq;
 public class MongoDBStorer {
 	public static void main(String[] args){
 		try {
-
-			//System.out.print(System.getProperty("user.dir"));
-			/*
-			HashMap<String, Dealership> dealerMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/dealerships.json"), HashMap.class);
-
-			List<Document> documents = new ArrayList<>();
-			
-			for(Map.Entry<String, Dealership> kv : dealerMap.entrySet()) {
-				Document doc = new Document();
-				doc.put("_id", kv.getKey());
-				doc.put("query", kv.getValue());
-
-				documents.add(doc);
-			}
-			
-			MongoClientURI uri = new MongoClientURI(
-				    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
-
-			MongoClient mongoClient = new MongoClient(uri);
-				
-			MongoDatabase db = mongoClient.getDatabase("The_Drag");
-			MongoCollection col = db.getCollection("dealerships");
-			col.insertMany(documents);*/
-			
-			/*
-			HashMap<String, Make> makeMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/makes.json"), HashMap.class);
-
-			List<Document> documents = new ArrayList<>();
-			
-			for(Map.Entry<String, Make> kv : makeMap.entrySet()) {
-				Document doc = new Document();
-				doc.put("_id", kv.getKey());
-				doc.put("query", kv.getValue());
-
-				documents.add(doc);
-			}
-			
-			MongoClientURI uri = new MongoClientURI(
-				    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
-
-			MongoClient mongoClient = new MongoClient(uri);
-				
-			MongoDatabase db = mongoClient.getDatabase("The_Drag");
-			MongoCollection col = db.getCollection("makes");
-			col.insertMany(documents);*/
-			
-			/*
-			HashMap<String, Car> carMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/cars.json"), HashMap.class);
-
-			List<Document> documents = new ArrayList<>();
-			
-			for(Map.Entry<String, Car> kv : carMap.entrySet()) {
-				Document doc = new Document();
-				doc.put("_id", kv.getKey());
-				doc.put("query", kv.getValue());
-
-				documents.add(doc);
-			}
-			
-			MongoClientURI uri = new MongoClientURI(
-				    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
-
-			MongoClient mongoClient = new MongoClient(uri);
-				
-			MongoDatabase db = mongoClient.getDatabase("The_Drag");
-			MongoCollection col = db.getCollection("cars");
-			col.insertMany(documents);*/
-			
-			MongoClientURI uri = new MongoClientURI(
-				    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
-
-			MongoClient mongoClient = new MongoClient(uri);
-				
-			MongoDatabase db = mongoClient.getDatabase("The_Drag");
-			
-			MongoCollection col = db.getCollection("makes");
-			
-			Document doc = (Document) col.find(eq("_id", "Honda")).first();
-			System.out.print(((Document)doc.get("query")).get("img"));
-			
-			//Object getMakeAttribute(String makeName, String attributeName);
-			//Object getDealershipAttribute(String dealerName, String attributeName);
+			storeDealerships();
+			storeMakes();
+			storeCars();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void storeDealerships() throws IOException, JsonParseException, JsonMappingException {
+		HashMap<String, Dealership> dealerMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/dealerships.json"), HashMap.class);
+
+		List<Document> documents = new ArrayList<>();
+		
+		for(Map.Entry<String, Dealership> kv : dealerMap.entrySet()) {
+			Document doc = new Document();
+			doc.put("_id", kv.getKey());
+			doc.put("query", kv.getValue());
+
+			documents.add(doc);
+		}
+		
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
+
+		MongoClient mongoClient = new MongoClient(uri);
+			
+		MongoDatabase db = mongoClient.getDatabase("The_Drag");
+		MongoCollection col = db.getCollection("dealerships");
+		col.insertMany(documents);
+	}
+	
+	private static void storeMakes() throws IOException, JsonParseException, JsonMappingException {
+		HashMap<String, Make> makeMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/makes.json"), HashMap.class);
+
+		List<Document> documents = new ArrayList<>();
+		
+		for(Map.Entry<String, Make> kv : makeMap.entrySet()) {
+			Document doc = new Document();
+			doc.put("_id", kv.getKey());
+			doc.put("query", kv.getValue());
+
+			documents.add(doc);
+		}
+		
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
+
+		MongoClient mongoClient = new MongoClient(uri);
+			
+		MongoDatabase db = mongoClient.getDatabase("The_Drag");
+		MongoCollection col = db.getCollection("makes");
+		col.insertMany(documents);
+	}
+	
+	private static void storeCars() throws IOException, JsonParseException, JsonMappingException {
+		HashMap<String, Car> carMap = new ObjectMapper().readValue(new File("src/main/java/web_scraping/cars.json"), HashMap.class);
+
+		List<Document> documents = new ArrayList<>();
+		
+		for(Map.Entry<String, Car> kv : carMap.entrySet()) {
+			Document doc = new Document();
+			doc.put("_id", kv.getKey());
+			doc.put("query", kv.getValue());
+
+			documents.add(doc);
+		}
+		
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://jdwalsh21:BI6SfPDyhGX8ihAU@thedragapiscrapes-2duen.gcp.mongodb.net/test?authSource=admin&replicaSet=TheDragAPIScrapes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true");
+
+		MongoClient mongoClient = new MongoClient(uri);
+			
+		MongoDatabase db = mongoClient.getDatabase("The_Drag");
+		MongoCollection col = db.getCollection("cars");
+		col.insertMany(documents);
 	}
 }
