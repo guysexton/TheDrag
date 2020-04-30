e8f86fcae887813ce597d2e1ab2f678fad9fb342
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-04-29 22:18:07 -0500

    Refactored Dealership.java Encapsulate Collection

--- src/main/java/web_scraping/Dealership.java
+++ src/main/java/web_scraping/Dealership.java
@@ -3,16 +3,16 @@ package web_scraping;
 import java.util.*;
 
 public class Dealership {
-	String name;
-	String img;
-	HashSet<String> makes = new HashSet<String>();
-	ArrayList<String> cars = new ArrayList<String>();
-	String address;
-	String phoneNum;
-	String website;
-	String hours;
-	String about;
-	
+	private String name;
+	private String img;
+	private HashSet<String> makes = new HashSet<String>();
+	private ArrayList<String> cars = new ArrayList<String>();
+	private String address;
+	private String phoneNum;
+	private String website;
+	private String hours;
+	private String about;
+
 	Dealership(){}
 	
 	Dealership(String name, String address, String phoneNum, String website, String hours, String about){
@@ -22,30 +22,6 @@ public class Dealership {
 		this.website = website;
 		this.hours = hours;
 		this.about = about;
-	}
-	
-	public String getImage() {
-		return img;
-	}
-	
-	public HashSet<String> getMakes() {
-		return makes;
-	}
-	
-	public ArrayList<String> getCars(){
-		return cars;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	
-	public String getWebsite() {
-		return website;
 	}
 	
 	public String toString() {
@@ -62,5 +38,77 @@ public class Dealership {
 	
 	public int hashCode() {
 		return name.hashCode() + address.hashCode();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public HashSet<String> getMakes() {
+		return makes;
+	}
+
+	public void setMakes(HashSet<String> makes) {
+		this.makes = makes;
+	}
+
+	public ArrayList<String> getCars() {
+		return cars;
+	}
+
+	public void setCars(ArrayList<String> cars) {
+		this.cars = cars;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getHours() {
+		return hours;
+	}
+
+	public void setHours(String hours) {
+		this.hours = hours;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
 	}
 }
